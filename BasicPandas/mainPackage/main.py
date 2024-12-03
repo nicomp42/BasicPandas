803d63f10291abf260a5cc41712fc1c78b89989f
nicomp42 <nicholdw@ucmail.uc.edu>
2024-12-03 07:24:12 -0500

    Add project files.

---
+++ BasicPandas/mainPackage/main.py
@@ -0,0 +1,12 @@
+# main.py
+# Bill Nicholson
+# nicholdw@ucmail.uc.edu
+
+from pandasDemoPackage.pandasDemo import pandasDemo
+
+def demo():
+    print("Pandas demo...")
+    myPandasDemo = pandasDemo()
+
+if __name__ == "__main__":
+    demo()
---
+++ BasicPandas/pandasDemoPackage/pandasDemo.py
@@ -0,0 +1,14 @@
+# pandasDemo.py
+# Bill Nicholson
+# nicholdw@ucmail.uc.edu
+
+import pandas as pd    # pd is the 'default' alias for the Pandas library
+
+class pandasDemo:
+    """
+    Demonstrate basic Pandas manipulation
+    """
+    def __init__(self):
+        pass
+    
+
